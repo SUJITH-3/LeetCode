1b4ba86113588fb26cc2b5c2754ab3452414f798
SUJITH S <sujithambur@gmail.com>
2023-12-23 12:32:15 +0530

    Update Contains Duplicate.java

--- Contains Duplicate.java
+++ Contains Duplicate.java
@@ -11,17 +11,13 @@ Output: false
 
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        int flag=0;
-        for(int i=0;i<nums.length;i++)
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++)
         {
-            for(int j=i+1;j<nums.length-1;j++)
+            if(nums[i]==nums[i+1])
             {
-                if(nums[i]==nums[j+1])
-                {
-                    return true;
-                }
+                return true;
             }
-         
         }
         return false;
         
