a50020158cc9c924eaf0d40d5be4333e1dc10f0c
SUJITH S <sujithambur@gmail.com>
2023-12-27 14:35:48 +0530

    Create Two Sum-2 Input Array is sorted.java

---
+++ Two Sum-2 Input Array is sorted.java
@@ -0,0 +1,31 @@
+Example 1:
+
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+Example 2:
+
+Input: numbers = [2,3,4], target = 6
+Output: [1,3]
+Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+Example 3:
+
+Input: numbers = [-1,0], target = -1
+Output: [1,2]
+Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
+
+
+  class Solution {
+    public int[] twoSum(int[] numbers, int target){
+        for(int i=0;i<numbers.length-1;i++)
+        {
+            for(int j=i+1;j<numbers.length;j++)
+            {
+                if(numbers[i]+numbers[j]==target)
+                return new int[]{i+1,j+1};
+            }
+        }
+        return new int[]{};
+        
+    }
+}
