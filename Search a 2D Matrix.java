c5bb23750b569ae215e14a791a3d1a0ae86390a6
SUJITH S <sujithambur@gmail.com>
2024-01-14 22:35:52 +0530

    Update Search a 2D Matrix.java

--- Search a 2D Matrix.java
+++ Search a 2D Matrix.java
@@ -9,18 +9,31 @@ Output: true
   Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
 Output: false
 
-  class Solution {
+class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m=matrix.length;
         int n=matrix[0].length;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++)
+        int start=0;
+        int end=m*n-1;
+        while(start<=end)
+        {
+            int mid=(start+end)/2;
+            int row=mid/n;
+            int col=mid%n;
+            int middleEle=matrix[row][col];
+            if(middleEle==target)
             {
-                if(matrix[i][j]==target)
                 return true;
             }
+            else if(middleEle<target)
+            {
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+
         }
         return false;
     }
 }
- 
