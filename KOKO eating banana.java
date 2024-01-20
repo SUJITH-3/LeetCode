f695dca57d4fbb902055c5ea526e0fc4b76b6aec
SUJITH S <sujithambur@gmail.com>
2024-01-20 18:17:48 +0530

    Update KOKO eating banana.java

--- KOKO eating banana.java
+++ KOKO eating banana.java
@@ -1,21 +1,40 @@
 class Solution {
+    int maxElement(int arr[])
+    {
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]>max)
+            max=arr[i];
+        }
+        return max;
+    }
+    int hourCalc(int mid,int arr1[])
+    {
+        double b=0;
+        for(int i=0;i<arr1.length;i++)
+        {
+          b=b+Math.ceil((double)arr1[i]/(double)mid);
+        }
+        return (int)b;
+    }
     public int minEatingSpeed(int[] piles, int h) {
-        int min = 1, max = 0;
-        for (int n : piles) if (max < n) max = n;
-
-        if (h == piles.length) return max;
-        while (min <= max) {
-            int tempH = 0, mid = (min + max) / 2;
-            for (int n : piles) {
-                tempH += n / mid;
-                if (n % mid > 0) tempH++;
+        int l=1;
+        int r=maxElement(piles);
+        while(l <= r)
+        {
+          
+            int mid= (l + r) /2;
+            int totalhr=hourCalc(mid,piles);
+            if(totalhr<=h)
+            {
+                r=mid-1;
             }
-            if (tempH <= h && tempH > 0) {
-                max = mid - 1;
-            } else if (tempH > h || tempH < 0) {
-                min = mid + 1;
+            else{
+                l=mid+1;
             }
         }
-        return min;
+       return l;
     }
+   
 }
